c64e50bb9b1a9f2c66a0efb51e6ad5fae2ccc8c1
Anto Idicherian Lonappan <antolonappan@icloud.com>
2024-08-31 20:09:20 -0700

    nsplit partially done
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- lat_cb/spectra.py
+++ lat_cb/spectra.py
@@ -15,7 +15,7 @@ class Spectra:
         fldname = ''
         if self.lat.atm_noise:
             fldname += '_atm'
-        self.lmax = 1000#3*nside-1
+        self.lmax = 3*self.lat.nside-1
         libdiri = os.path.join(libdir,f'spectra_{self.nside}'+fldname)
         comdir = os.path.join(libdir,f'spectra_{self.nside}'+'_common')
         self.__set_dir__(libdiri,comdir)
